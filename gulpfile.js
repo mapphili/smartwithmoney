3216367bf59d108ad6434b40e58331bff23613da
pphilii <p.philippens@student.tue.nl>
2016-12-06 14:19:49 +0100

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,22 @@
+
+function Calculate() {
+	var savinggoal = document.getElementById("SavingGoal").value;
+	var currentmoney = document.getElementById("CurrentMoney").value;
+	var savemonth = document.getElementById("SaveMonth").value;
+	var savingmoney = parseFloat(savinggoal) - parseFloat(currentmoney);
+	var moneyleft = savingmoney / parseFloat(savemonth);
+	console.log(moneyleft);
+	//return Math.abs(moneyleft);
+	
+	var output = document.getElementById("money_output");
+	if (savinggoal == "" || currentmoney == "" || savemonth == "") {
+			alert("Please fill in all fields.");
+	} else if (isNaN(savinggoal) || isNaN(currentmoney) || isNaN(savemonth)) {
+			alert("Please fill in numbers.")
+	} else if (currentmoney > savinggoal) {
+			output.innerHTML = "Gongrats! You have already reached your saving goal!";
+	} else {
+	output.innerHTML = "You still have " + Math.ceil(moneyleft) + " months left to save.";
+	}
+}
+
